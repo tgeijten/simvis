8cc074e5cf2c1acf27faec19c465344355919383
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2016-07-21 11:03:37 +0200

    added osg_camera_man class to simvis

---
+++ simvis/osg_camera_man.cpp
@@ -0,0 +1,58 @@
+#include "osg_camera_man.h"
+#include "scone/core/math.h"
+
+using namespace osg;
+
+namespace vis
+{
+	osg_camera_man::osg_camera_man() :
+		osgGA::OrbitManipulator(),
+		orbit_yaw( 0 ),
+		orbit_pitch( -5 )
+	{
+		setAllowThrow( false );
+		_distance = 5;
+		_center = Vec3d( 0, 1, 0 );
+
+		updateRotation();
+
+		osg::Vec3d eye, center, up;
+		getTransformation( eye, center, up );
+		setHomePosition( eye, center, up );
+	}
+
+	osg_camera_man::~osg_camera_man() {}
+
+	bool osg_camera_man::performMovementLeftMouseButton( const double eventTimeDelta, const double dx, const double dy )
+	{
+		orbit_pitch += degree( pitch_scale * dy );
+		orbit_yaw -= degree( yaw_scale * dx );
+
+		updateRotation();
+
+		return true;
+	}
+
+	void osg_camera_man::updateRotation()
+	{
+		auto yaw = osg::Quat( orbit_yaw.rad_value(), osg::Vec3d( 0, 1, 0 ) );
+		auto pitch = osg::Quat( orbit_pitch.rad_value(), osg::Vec3d( 1, 0, 0 ) );
+		_rotation = pitch * yaw;
+	}
+
+	bool osg_camera_man::performMovementMiddleMouseButton( const double eventTimeDelta, const double dx, const double dy )
+	{
+		zoomModel( dy * zoom_scale, false );
+
+		return true;
+	}
+
+	bool osg_camera_man::performMovementRightMouseButton( const double eventTimeDelta, const double dx, const double dy )
+	{
+		// pan model
+		float scale = -pan_scale * _distance;
+		panModel( dx * scale, dy * scale );
+
+		return true;
+	}
+}
---
+++ simvis/osg_camera_man.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "prerequisites.h"
+#include <osgGA/OrbitManipulator>
+#include "flut/math/angle.hpp"
+
+namespace vis
+{
+	using degree = flut::math::degree;
+
+	class SIMVIS_API osg_camera_man : public osgGA::OrbitManipulator
+	{
+	public:
+		osg_camera_man();
+		virtual ~osg_camera_man();
+
+	protected:
+		virtual bool performMovementLeftMouseButton( const double eventTimeDelta, const double dx, const double dy );
+		virtual bool performMovementMiddleMouseButton( const double eventTimeDelta, const double dx, const double dy );
+		virtual bool performMovementRightMouseButton( const double eventTimeDelta, const double dx, const double dy );
+		void updateRotation();
+
+	private:
+		degree orbit_yaw;
+		degree orbit_pitch;
+
+		double pitch_scale = 100;
+		double yaw_scale = 100;
+		double pan_scale = 0.3;
+		double zoom_scale = 1.0;
+	};
+}
