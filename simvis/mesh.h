3f1c3170aa21c4d496bd8d56fa3605cdbf5af585
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-02-26 17:34:29 +0100

    F: regression because of git

--- simvis/mesh.h
+++ simvis/mesh.h
@@ -12,7 +12,7 @@ namespace vis
 		mesh() : node( nullptr ) {}
 		mesh( const mesh& other ) : node( other ) {}
 		mesh( node& parent, const xo::path& filename );
-		mesh( node& parent, const xo::shape& shape, const color& col, float detail );
+		mesh( node& parent, const xo::shape& shape, const color& col, float detail = default_detail );
 		virtual ~mesh();
 
 		mesh& scale( const vec3f& s );
